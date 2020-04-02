d189497006d268b1f4333410399c1c620c57d0f2
Ayush_Mann <learnertsk@gmail.com>
2020-04-02 17:51:38 +0530

    one side drag and drop implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+   tasks: [{name:"Learn Angular",category:"wip", bgcolor: "yellow"},
+            {name:"React", category:"wip", bgcolor:"pink"},
+            {name:"Vue", category:"complete", bgcolor:"skyblue"}]
+  }
+  
+  onDragOver = (event)=>{
+    event.preventDefault()
+  }         
+  onDrop =(event, category) =>{
+    let id = event.dataTransfer.getData('id')
+    let tasks = this.state.tasks.filter((task)=>{
+      if(task.name == id){
+        task.category = category
+      }
+      return task
+    })
+    this.setState({
+      ...this.state,
+    })
+  }
+  onDragStart = (event,name) =>{
+    console.log("name dragStart", name)
+    event.dataTransfer.setData("id",name)
+  }
+  render(){
+    var tasks = {
+      wip: [],
+      complete: []
+  }
+
+  this.state.tasks.forEach ((t) => {
+      tasks[t.category].push(
+          <div key={t.name}
+              onDragStart={(e)=>this.onDragStart(e, t.name)} //sending name as an unique value to idetify it .
+              draggable
+              className="draggable"
+              style = {{backgroundColor: t.bgcolor,padding:"20px"}}>
+              {t.name}
+          </div>
+      );
+  });
+    return(
+      <>
+      <h2>Drag and Drop</h2>
+      <div className="container-drag" style={{display:"flex"}}>
+        <div className="wip" 
+          onDrop={(e)=>this.onDrop(e, "wip")}
+          style={{marginRight:"10px"}}>
+          <span>WIP</span>
+          {tasks.wip}
+        </div>
+        <div className="droppable"
+             onDragOver={(e)=>this.onDragOver(e)}
+             onDrop={(e)=>this.onDrop(e, 'complete')}>
+            <span>COMPLETED</span>
+            {tasks.complete}
+        </div>
+      </div>
+      </>
+    )
+  }
 }
 
 export default App;
